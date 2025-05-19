3b672fd3c9abd4e5a1492b16770a358c2eee3d39
itspirates635 <anishpoudel335@gmail.com>
2025-05-19 19:47:51 +0545

    Database and MySqlConnection classes

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -2,12 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Database;
-
+package database;
+import java.sql.*;
 /**
  *
  * @author anish
  */
-public class Database {
-    
+public interface Database {
+    Connection openConnection();
+    void closeConnection(Connection conn);
+    ResultSet runQuery(Connection conn, String query); //reference type
+    int executeUpdate(Connection conn, String query);
 }
--- src/moviemate/MovieMate.java
+++ src/moviemate/MovieMate.java
@@ -1,20 +1,20 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package moviemate;
-
+import database.*;
 /**
  *
  * @author anish
  */
 public class MovieMate {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Database db = (Database) new MySqlConnection();
+        if(db.openConnection() != null){
+            System.out.println("Database connected Sucessfully");
+        }else{
+            System.out.println("Failed to connect to database");
+        }
     }
-    
 }
