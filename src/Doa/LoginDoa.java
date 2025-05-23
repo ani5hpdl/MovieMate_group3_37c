dcc8019eebf4ccd1b643984dd476e55224b7ea6e
SejalPaudel <sejupaudel@gmail.com>
2025-05-23 12:12:18 +0545

    Changed getuserName to getEmail

--- src/Doa/LoginDoa.java
+++ src/Doa/LoginDoa.java
@@ -3,26 +3,31 @@ package Doa;
 import Model.Login;
 import java.sql.Connection;
 import database.MySqlConnection;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.*;
 
 public class LoginDoa {
     MySqlConnection mysql= new MySqlConnection();
     
-    public void signin(Login user){
+    public Login signin(Login loginRequest){
         Connection conn = mysql.openConnection();
-        
-        String sql = "INSERT INTO users(userName, password)VLAUES (?,?)";
-        try(PreparedStatement pstm = conn.prepareStatement(sql) ){
-            pstm.setString(1, user.getUserName());
-            pstm.setString(2,user.getPassword());
-            pstm.executeUpdate();
+        String sql = "SELECT * FROM users where userName = ? and password = ?";
+        try(PreparedStatement pstmt = conn.prepareStatement(sql)){
+            pstmt.setString(1, loginRequest.getEmail());
+            pstmt.setString(2, loginRequest.getPassword());
+            ResultSet result = pstmt.executeQuery();
         }catch(SQLException ex){
-            Logger.getLogger(LoginDoa.class.getName()).log(Level.SEVERE, null,ex);
-        }finally {
+            System.out.println(ex);
+        }finally{
             mysql.closeConnection(conn);
         }
+        return null;
     }
+    
+    
+        
+    
+
+    
 }
