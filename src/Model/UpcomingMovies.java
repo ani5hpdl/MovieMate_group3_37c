f8afa208681dc670792518d2d180f96d0d0f86f6
jelisaaa <subedijelisa@gmail.com>
2025-06-13 22:23:04 +0545

    UpcomingMovies Model Created

---
+++ src/Model/UpcomingMovies.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author it solution
+ */
+public class UpcomingMovies {
+     private String title;
+    private String releaseDate;
+    private String imagePath;  // Path to poster/image
+    private String description;
+
+}
