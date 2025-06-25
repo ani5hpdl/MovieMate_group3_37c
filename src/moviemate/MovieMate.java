84ab22c8de98bb61f8595099c21753af058fb3bb
SejalPaudel <sejupaudel@gmail.com>
2025-06-25 21:09:01 +0545

    Completed

--- src/moviemate/MovieMate.java
+++ src/moviemate/MovieMate.java
@@ -3,18 +3,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package moviemate;
+import Controller.BookingHistoryController;
 import database.*;
+import view.bookinghistory;
 /**
  *
  * @author anish
  */
 public class MovieMate {
+    
+    public static int loogedInUserId=1; //hardcoded for now, remove it later
+    
     public static void main(String[] args) {
-        Database db = (Database) new MySqlConnection();
-        if(db.openConnection() != null){
-            System.out.println("Database connected Sucessfully");
-        }else{
-            System.out.println("Failed to connect to database");
-        }
+//        Database db = (Database) new MySqlConnection();
+//        if(db.openConnection() != null){
+//            System.out.println("Database connected Sucessfully");
+//        }else{
+//            System.out.println("Failed to connect to database");
+//        }
+    
+//    int realUserId = loginController.getLoggedInUserId(); (for when passing realUserID, decomment it)
+
+    bookinghistory bookingView = new bookinghistory();
+    BookingHistoryController bookingController = new BookingHistoryController(bookingView, loogedInUserId);
+    bookingController.open();
     }
 }
