cda3e637f92e99f12e31e5186d28de61091c4214
Nitish <nitishstha15@gmail.com>
2025-07-02 21:20:37 +0545

    TicketModel Complete

---
+++ src/Model/TicketModel.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author lenovo
+ */
+public class TicketModel {
+
+
+    private String movieName;
+    private String duration;
+    private String date;
+    private String time;
+    private String seatNo;
+    private String ticketId;
+    private String theaterNo;
+    private String vat;
+    private String totalAmount;
+
+    public TicketModel() {
+    }
+    
+    public static void main(String[] args) {
+                System.out.println("This is the main method. You can run our code here.");
+    }
+
+    
+    public TicketModel(String movieName, String duration, String date, String time, String seatNo, String ticketId, String theaterNo, String vat, String totalAmount) {
+        this.movieName = movieName;
+        this.duration = duration;
+        this.date = date;
+        this.time = time;
+        this.seatNo = seatNo;
+        this.ticketId = ticketId;
+        this.theaterNo = theaterNo;
+        this.vat = vat;
+        this.totalAmount = totalAmount;
+    }
+
+    // Getters and Setters
+    public String getMovieName() {
+        return movieName;
+    }
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getSeatNo() {
+        return seatNo;
+    }
+    public void setSeatNo(String seatNo) {
+        this.seatNo = seatNo;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getTheaterNo() {
+        return theaterNo;
+    }
+    public void setTheaterNo(String theaterNo) {
+        this.theaterNo = theaterNo;
+    }
+
+    public String getVat() {
+        return vat;
+    }
+    public void setVat(String vat) {
+        this.vat = vat;
+    }
+
+    public String getTotalAmount() {
+        return totalAmount;
+    }
+    public void setTotalAmount(String totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+}
